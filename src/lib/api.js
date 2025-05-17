1e8cac4c157ee310685f379d559569e17dd81bb3
sethu-ravichandran <rsethu1997@gmail.com>
2025-05-18 02:28:34 +0530

    Fix. Added the backend url.

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === "production"
-    ? "/api" // Production API path
+    ? `${import.meta.env.VITE_BACKEND_URL}/api` // Production API path
     : "http://localhost:3500/api", // Development server
   headers: {
     "Content-Type": "application/json",
